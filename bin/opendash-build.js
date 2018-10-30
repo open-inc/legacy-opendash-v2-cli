069f3aaea255612e89794a2a4c1152c486dbafd1
Sebastian Zilles <dev@zilles.io>
2018-10-30 13:34:31 +0100

    2.0.0-beta

--- bin/opendash-build.js
+++ bin/opendash-build.js
@@ -1,96 +1,53 @@
-const path = require('path');
 const fs = require('fs-extra');
 
 const program = require('commander');
-const inquirer = require('inquirer');
 
-const webpack = require('webpack');
-const server = require('live-server');
-const pkgAssets = require('pkg-assets');
+const webpack = require('../lib/webpack');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-
-const autoprefixer = require('autoprefixer');
-
-const call = require('../lib/call');
 const cwd = require('../lib/cwd');
 
-function logPath(x) {
-  return x.replace(cwd('.'), '.');
-}
-
-function testJsFiles(x) {
-  if (/\.js$/.test(x)) {
-    const pathRel = path.normalize(x).replace(cwd(''), '');
-    if (/(node_modules)/.test(x) && !(/node_modules[\/|\\]@?opendash[a-zA-Z0-9\-]*[\/|\\]/.test(pathRel))) { // eslint-disable-line
-      return false;
-    }
-    return true;
-  }
-  return false;
-}
-
 program
-  .option('-t, --type <type>', 'Either \'Module\' or \'Instance\'.')
-  .option('-c, --clean', 'Clean old dist folder.')
+  .option('-t, --type <type>', "Either 'Module' or 'Instance'.")
+  // .option('-c, --clean', 'Clean old dist folder.')
   .option('-w, --watch', 'Watch the source files for changes.')
   .option('-s, --serve', 'Serve the dist folder.')
   .option('-o, --open', 'Open browser if serve mode is on.')
   .option('-m, --minify', 'Output will be minified.')
-  .option('-p, --port <port>', 'Define a port, if serve mode is on. Default is 8080.')
-  .option('--source-map <setting>', 'Define the source-map type, see webpack.js.org/configuration/devtool')
-  .option('--public-path <url>', 'Set a public path, see webpack.js.org/concepts/output')
+  .option(
+    '-p, --port <port>',
+    'Define a port, if serve mode is on. Default is 8080.',
+  )
+  .option(
+    '--source-map <setting>',
+    'Define the source-map type, see webpack.js.org/configuration/devtool',
+  )
+  .option(
+    '--public-path <url>',
+    'Set a public path, see webpack.js.org/concepts/output',
+  )
   .parse(process.argv);
 
-// const config = {};
+async function init() {
+  const DEFAULTS = {
+    watch: program.watch || false,
+    serve: program.serve || false,
+    // clean: program.clean || false,
+    minify: program.minify || false,
+    open: program.open || false,
+    port: program.port || 8080,
+    sourceMap: program.sourceMap || false,
+    publicPath: program.publicPath || false,
+  };
 
-const TYPES = [
-  'Instance',
-  'Module',
-];
-
-const DEFAULTS = {
-  watch: program.watch || false,
-  serve: program.serve || false,
-  clean: program.clean || false,
-  minify: program.minify || false,
-  open: program.open || false,
-  port: program.port || 8080,
-  sourceMap: program.sourceMap || false,
-  publicPath: program.publicPath || false,
-};
-
-const configPath = cwd('.opendashrc.json');
-
-call(async () => {
-  const questions = [];
+  const configPath = cwd('.opendashrc.json');
 
   let options = {};
 
-  if (await fs.pathExists(configPath)) {
-    Object.assign(options, await fs.readJson(configPath));
+  if (!(await fs.pathExists(configPath))) {
+    throw new Error('Not an open.DASH instance.');
   }
 
-  if (program.type) {
-    options.type = program.type;
-  }
-
-  if (!options.type || TYPES.indexOf(options.type) === -1) {
-    questions.push({
-      type: 'list',
-      name: 'type',
-      message: 'What do you want to build?',
-      choices: [
-        'Instance',
-        'Module',
-      ],
-    });
-  }
-
-  if (questions.length > 0) Object.assign(options, await inquirer.prompt(questions));
+  // Object.assign(options, await fs.readJson(configPath));
 
   options.dist = options.dist || 'dist';
   options.index = options.index || 'app/index.html';
@@ -100,191 +57,9 @@ call(async () => {
 
   options = Object.assign({}, DEFAULTS, options);
 
-  const WEBPACK_CONFIG = {
-    entry: {
-      app: [require.resolve('babel-polyfill'), cwd(options.entry)],
-    },
-    output: {
-      filename: '[name].js', // '[name].[chunkhash].js',
-      path: cwd(options.dist),
-    },
-    module: {
-      rules: [
-        {
-          test: testJsFiles,
-          use: [{
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                [
-                  require.resolve('babel-preset-env'),
-                  {
-                    targets: {
-                      browsers: [
-                        'last 2 Chrome versions',
-                        'ie >= 11',
-                      ],
-                    },
-                  },
-                ],
-                // require.resolve('babel-preset-stage-2'),
-              ],
-              babelrc: false,
-            },
-          }],
-        },
-        {
-          test: /\.css$/,
-          use: ExtractTextPlugin.extract({
-            use: ['css-loader'],
-          }),
-        },
-        {
-          test: /\.scss$/,
-          use: ExtractTextPlugin.extract({
-            use: [
-              {
-                loader: 'css-loader',
-                options: {
-                  minimize: true,
-                },
-              },
-              {
-                loader: 'postcss-loader',
-                options: {
-                  plugins: () => [
-                    autoprefixer(),
-                  ],
-                },
-              },
-              {
-                loader: 'sass-loader',
-                options: {
-                },
-              },
-            ],
-          }),
-        },
-        {
-          test: /\.html$/,
-          use: [{
-            loader: 'html-loader',
-            options: {
-              minimize: true,
-              removeComments: true,
-              collapseWhitespace: true,
-            },
-          }],
-        },
-        {
-          test: /\.(png|jpg|gif)$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                name: '[hash]-[name].[ext]',
-                // outputPath: 'assets/',
-              },
-            },
-          ],
-        },
-      ],
-    },
-    plugins: [
-      new webpack.optimize.CommonsChunkPlugin({
-        name: 'vendor',
-        minChunks: mod => mod.context && mod.context.indexOf('node_modules') !== -1,
-      }),
-      new webpack.optimize.CommonsChunkPlugin({
-        name: 'manifest',
-      }),
-      new ExtractTextPlugin({
-        filename: 'styles/[name].css',
-      }),
-      new OptimizeCSSPlugin({
-        cssProcessorOptions: {
-          safe: true,
-        },
-      }),
-      new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: cwd(options.index),
-        inject: true,
-        chunksSortMode: 'dependency',
-        hash: false,
-        minify: {
-          removeComments: true,
-          collapseWhitespace: true,
-          removeAttributeQuotes: true,
-        },
-      }),
-    ],
-    resolveLoader: {
-      modules: [path.resolve(__dirname, '..', 'node_modules'), cwd('node_modules')],
-      extensions: ['.js', '.json'],
-      mainFields: ['loader', 'main'],
-    },
-    devtool: DEFAULTS.sourceMap,
-    watch: false,
-    watchOptions: {
-      aggregateTimeout: 300,
-      poll: 500,
-    },
-  };
-
-  if (options.clean) {
-    console.log('Starting in clean-mode: Deleting old dist folder.');
-    await fs.remove(WEBPACK_CONFIG.output.path);
-  }
-
-  if (options.watch) {
-    console.log('Starting in watch-mode: Watching source code for changes.');
-    WEBPACK_CONFIG.watch = true;
-  }
-
-  if (options.minify) {
-    console.log('Starting in minify-mode: Output will be minified.');
-    WEBPACK_CONFIG.plugins.push(new UglifyJsPlugin({
-      sourceMap: (options.sourceMap) ? true : false, // eslint-disable-line
-    }));
-  }
-
-  if (options.publicPath) {
-    console.log('Starting in public-path-mode: ...');
-    WEBPACK_CONFIG.output.publicPath = options.publicPath;
-  }
-
-  if (options.serve) {
-    const serverOptions = {
-      root: WEBPACK_CONFIG.output.path,
-      port: options.port,
-      open: options.open,
-      logLevel: 0,
-      wait: 500,
-    };
-
-    server.start(serverOptions);
-
-    console.log(`Starting in serve-mode: Serving '${logPath(serverOptions.root)}' at 127.0.0.1:${serverOptions.port}`);
-  }
-
   console.log('');
 
-  const assets = await pkgAssets(process.cwd(), `${WEBPACK_CONFIG.output.path}/assets/vendor`);
+  await webpack(options);
+}
 
-  assets.map(asset => console.log(`Asset(s) moved: ${logPath(asset.src)} -> ${logPath(asset.dest)}`));
-
-  webpack(WEBPACK_CONFIG, (err, stats) => {
-    if (err) throw err;
-
-    const statString = stats.toString({
-      colors: true,
-      modules: false,
-      children: false,
-      chunks: false,
-      chunkModules: false,
-    });
-
-    process.stdout.write(`\n${statString}\n\n`);
-  });
-});
+init().then(null, error => console.error(error));
--- bin/opendash.js
+++ bin/opendash.js
@@ -1,26 +1,14 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const semver = require('semver');
-const latest = require('latest-version');
 const pkg = require('../package.json');
-const call = require('../lib/call');
 
-call(async () => {
-  console.log(`open.DASH CLI v${pkg.version}`);
+console.log(`open.DASH CLI v${pkg.version}`);
 
-  const remoteVersion = await latest('opendash-cli');
+program
+  .version(pkg.version)
 
-  if (semver.gt(remoteVersion, pkg.version)) {
-    console.log(`There is a new version (${remoteVersion}) of opendash-cli available, install with:\n  npm i -g opendash-cli`);
-  }
+  .command('init <template> <name>', 'Initiate a new instance or widget.')
+  .command('build', 'Build an Instance or a Widget.')
 
-  program
-    .version(pkg.version)
-
-    .command('init <template> <name>', 'Initiate a new instance or widget.')
-    .command('build', 'Build an Instance or a Widget.')
-    .command('serve [path]', 'Starts a dev server in the current directory and on the given port.')
-
-    .parse(process.argv);
-});
+  .parse(process.argv);
